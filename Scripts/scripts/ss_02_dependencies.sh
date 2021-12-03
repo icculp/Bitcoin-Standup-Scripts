bbbdf16dc1c64ec89102024c2d51a20f8e4e3639
Ian Culp <icculp@gmail.com>
2021-12-03 09:27:40 -0600

    Adding dependencies script

---
+++ Scripts/scripts/ss_02_dependencies.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# standup.sh
+
+
+# Output stdout and stderr to ~root files
+exec > >(tee -a /root/standup.log) 2> >(tee -a /root/standup.log /root/standup.err >&2)
+
+# Make sure all packages are up-to-date
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install gnupg -y
